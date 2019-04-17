7e244c369837b9951d2b5af1e16dda3b95ed667e
fengxie98 <46989561+fengxie98@users.noreply.github.com>
2019-04-17 10:27:17 -0700

    Update SFM Tutorial.cpp
    Added 2 player, fixed increase of game speed upon fruit eaten

--- SFM Tutorial.cpp
+++ SFM Tutorial.cpp
@@ -18,12 +18,19 @@ int h = size * num_vertBox;    //background number of pixels in height
 int direction;    //direction the snake is moving 
 int snake_length = 4; //initial size is 4 
 
+int direction2;
+int snake_length2 = 4;
+
 //Actual Snake in game is an array of x,y coordinates of sprite2
 struct Snake
 {
 	int x, y;        //each sprite of snake needs an x,y coordinate
 }s[100];
 
+struct Snake2 {
+	int x, y;
+	
+}sTwo[100];
 //***NEW*** this is the fruit or food that the snake will eat
 struct Fruit
 {
@@ -40,6 +47,12 @@ void move() {
 	{
 		s[i].x = s[i - 1].x;
 		s[i].y = s[i - 1].y;
+	}
+
+	for (int i = snake_length2; i > 0; i--)
+	{
+		sTwo[i].x = sTwo[i - 1].x;
+		sTwo[i].y = sTwo[i - 1].y;
 	}
 	//2nd update the head
 	//Head of snake, s[0] depends on direction user inputs 
@@ -58,6 +71,23 @@ void move() {
 	//if user presses right
 	if (direction == 3)
 		s[0].x += 1;
+	
+	//Snake2
+	if (direction2 == 0)
+		sTwo[0].y -= 2;
+
+	//if user presses down
+	if (direction2 == 1)
+		sTwo[0].y += 2;
+
+	//if user presses left
+	if (direction2 == 2)
+		sTwo[0].x -= 2;
+
+	//if user presses right
+	if (direction2 == 3)
+		sTwo[0].x += 2;
+
 
 
 	//***NEW*** If Snake eats food it should grow
@@ -67,7 +97,19 @@ void move() {
 		//increment snake
 		snake_length++;
 
-		delay = delay - 0.1; //game faster when fruit eat
+		delay = delay - 0.01f; //game faster when fruit eat
+
+		//Randomly place food somewhere else
+		food.x = rand() % num_horzBox;
+		food.y = rand() % num_vertBox;
+	}
+
+	if (((sTwo[0].x) == food.x) && ((sTwo[0].y) == food.y))
+	{
+		//increment snake
+		snake_length2++;
+
+		delay = delay - 0.01f; //game faster when fruit eat
 
 		//Randomly place food somewhere else
 		food.x = rand() % num_horzBox;
@@ -87,12 +129,37 @@ void move() {
 	if (s[0].y < 0)
 		s[0].y = num_vertBox;
 
+	if (sTwo[0].x > num_horzBox)
+		sTwo[0].x = 0;
+	if (sTwo[0].x < 0)
+		sTwo[0].x = num_horzBox;
+	//TOP and BOTTOM
+	if (sTwo[0].y > num_vertBox)
+		sTwo[0].y = 0;
+	if (sTwo[0].y < 0)
+		sTwo[0].y = num_vertBox;
+
 	//***NEW*** Check if you eat body of snake
 	for (int i = 1; i < snake_length; i++)
 	{
 		//Cut Snake body from place eaten
-		if (s[0].x == s[i].x && s[0].y == s[i].y)
+		if (s[0].x == s[i].x && s[0].y == s[i].y) {
 			snake_length = i;
+			
+			delay = .1f;
+		}
+		
+	}
+
+	for (int i = 1; i < snake_length2; i++)
+	{
+		//Cut Snake body from place eaten
+		if (sTwo[0].x == sTwo[i].x && sTwo[0].y == sTwo[i].y) {
+			snake_length2 = i;
+
+			delay = .1f;
+		}
+
 	}
 }
 
@@ -105,16 +172,17 @@ int main()
 	sf::RenderWindow window(sf::VideoMode(w, h), "Snake Game");
 
 	//Textures load an image into the GPU Memory
-	sf::Texture t1, t2, t3;
+	sf::Texture t1, t2, t3, t4;
 	t1.loadFromFile("white.png");
 	t2.loadFromFile("red.png");
 	t3.loadFromFile("red.png"); //change image to red
-
+	t4.loadFromFile("green.png");
 	//Sprite has physical dimmensions that can be set in 
 	//coordinate system, setPosition(x,y), and drawn on screen
 	sf::Sprite sprite1(t1);
 	sf::Sprite sprite2(t2);
 	sf::Sprite sprite3(t3);
+	sf::Sprite sprite4(t4);
 
 	//***NEW*** initially place food somewhere on screen
 	food.x = 10;
@@ -134,13 +202,14 @@ int main()
 		sf::Event e;
 
 		//Check when the window is closed
-		while (window.pollEvent(e))
-		{
-			//If user presses x in the top right, Windows, top left, Mac,  close the window
+		while (window.pollEvent(e)) {
 			if (e.type == sf::Event::Closed)
-			{
-				window.close();
-			}
+				while (window.pollEvent(e)) {
+					//If user presses x in the top right, Windows, top left, Mac,  close the window
+					{
+						window.close();
+					}
+				}
 		}
 
 		//Control for Snake by User
@@ -148,6 +217,11 @@ int main()
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) direction = 1;
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) direction = 2;
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) direction = 3;
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) direction2 = 0;
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) direction2 = 1;
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) direction2 = 2;
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) direction2 = 3;
 
 		if (timer > delay)
 		{
@@ -180,6 +254,14 @@ int main()
 			sprite2.setPosition(s[i].x*size, s[i].y*size);
 			//Draw sprite2 one at a time by drawing over background
 			window.draw(sprite2);
+		}
+
+		for (int i = 0; i < snake_length2; i++)
+		{
+			//position sprite2 one at a time
+			sprite4.setPosition(sTwo[i].x*size, sTwo[i].y*size);
+			//Draw sprite2 one at a time by drawing over background
+			window.draw(sprite4);
 		}
 
 		//***NEW*** 3rd: Draw Fruit
