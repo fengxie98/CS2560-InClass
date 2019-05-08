1150094fb38e4f8c4ae34d778f803cfd906165fd
fengxie98 <46989561+fengxie98@users.noreply.github.com>
2019-05-07 20:32:36 -0700

    Add files via upload

---
+++ GoogleUnitTest.cpp
@@ -0,0 +1,10 @@
+// GoogleUnitTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <gtest/gtest.h>
+
+TEST(GoogleUnitTest, AssertTrue) {
+	ASSERT_TRUE(true);
+}
